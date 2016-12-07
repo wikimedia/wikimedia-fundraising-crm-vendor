b861ec091301115a10c0efb5969b869f861e6ef7
Elliott Eggleston <ejegg@ejegg.com>
2016-12-06 18:07:06 -0600

    Update SmashPig library
    Change-Id: Ifa66a5822a66939cb0885bfbd9c4b501c1a12e6d

--- wikimedia/smash-pig/PaymentProviders/Amazon/Tests/phpunit/ApiTest.php
+++ wikimedia/smash-pig/PaymentProviders/Amazon/Tests/phpunit/ApiTest.php
@@ -1,29 +1,14 @@
 <?php
 namespace SmashPig\PaymentProviders\Amazon\Tests;
 
-use SmashPig\Core\Context;
 use SmashPig\PaymentProviders\Amazon\AmazonApi;
-use SmashPig\Tests\BaseSmashPigUnitTestCase;
 
-class ApiTest extends BaseSmashPigUnitTestCase {
-
-	protected $mockClient;
-
-	public function setUp() {
-		parent::setUp();
-		chdir( __DIR__ . '/..' ); // So the mock client can find its response files
-		$config = AmazonTestConfiguration::instance();
-		Context::initWithLogger( $config );
-		$this->mockClient = $config->object( 'payments-client', true );
-		$this->mockClient->calls = array();
-		$this->mockClient->returns = array();
-		$this->mockClient->exceptions = array();
-	}
+class ApiTest extends AmazonTestCase {
 
 	public function testFindParent() {
 		$this->mockClient->returns['getAuthorizationDetails'][] = 'Declined';
 		$this->mockClient->returns['getAuthorizationDetails'][] = 'Closed';
-		$parentId = AmazonApi::findRefundParentId( 'P01-0133129-0199515-R019658' );
+		$parentId = AmazonApi::get()->findCaptureId( 'P01-0133129-0199515' );
 		$this->assertEquals( 'P01-0133129-0199515-C019658', $parentId, 'Did not get the right refund parent ID' );
 	}
 }
