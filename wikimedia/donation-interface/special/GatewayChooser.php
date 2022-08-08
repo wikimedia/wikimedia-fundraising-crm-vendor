53baeca1c1362cd661a63707d693b88ef0fe580a
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-08-09 10:16:01 +1200

    Update donation-interface to 2.5.7.5, smashpig to 0.8.2.2
    Change-Id: Ib6e07aa6feaf4a8cf7b1f2f1db82d734a1313769

--- wikimedia/donation-interface/special/GatewayChooser.php
+++ wikimedia/donation-interface/special/GatewayChooser.php
@@ -346,13 +346,20 @@ class GatewayChooser extends UnlistedSpecialPage {
 		// For recurring, we'll interpret no URL param, 'false', '0' and '' as false.
 		// This follows legacy behavior, to ensure existing links work as expected.
 
-		// sanitizedValOrNull() will return null if the param is absent, '' or '0'
+		// sanitizedValOrNull() will return null if the param is absent or ''
 		$recurringRawVal = $this->sanitizedValOrNull( 'recurring' );
+
+		// We map this to 0 or 1 rather than boolean true or false because the
+		// getLocalURL function we eventually feed this to will discard any param
+		// whose value is false.
 		if ( $recurringRawVal === 'false' ) {
-			$recurring = false;
+			$recurring = 0;
+		} elseif ( $recurringRawVal === 'true' ) {
+			$recurring = 1;
 		} else {
-			// map null to explicitly false
-			$recurring = (bool)$recurringRawVal;
+			// map null to 0, as we want to affirmatively overwrite any recurring=1
+			// from a previous attempt if there is no recurring value on this URL.
+			$recurring = (int)$recurringRawVal;
 		}
 
 		// These are the parameters that are actually used to find possible gateways
@@ -380,7 +387,7 @@ class GatewayChooser extends UnlistedSpecialPage {
 
 	/**
 	 * Get the sanitized string value of a URL parameter. If the parameter was not present
-	 * or is an empty string (or, more precisely, is empty()), return null.
+	 * or is an empty string, return null.
 	 *
 	 * @param string $paramName
 	 * @return ?string
@@ -388,7 +395,7 @@ class GatewayChooser extends UnlistedSpecialPage {
 	private function sanitizedValOrNull( string $paramName ): ?string {
 		$val = $this->getRequest()->getVal( $paramName, null );
 
-		if ( empty( $val ) ) {
+		if ( $val === '' ) {
 			return null;
 		}
 
