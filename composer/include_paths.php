0167e9d41097a4eb46376f8160f58caff4b5c354
eileen <emcnaughton@wikimedia.org>
2025-11-19 16:03:08 +1300

    Upgrading wikimedia/smash-pig (v0.9.12.6 => v0.9.12.7
    Change-Id: I71e8f113b4dd5d5cd3d25d79087cf73f942abec3

--- composer/include_paths.php
+++ composer/include_paths.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     $vendorDir . '/pear/auth_sasl',
     $vendorDir . '/pear/console_getopt',
+    $vendorDir . '/pear/db',
     $vendorDir . '/pear/log',
     $vendorDir . '/pear/mail',
     $vendorDir . '/pear/mail_mime',
@@ -16,5 +17,4 @@ return array(
     $vendorDir . '/pear/pear-core-minimal/src',
     $vendorDir . '/pear/pear_exception',
     $vendorDir . '/pear/validate_finance_creditcard',
-    $vendorDir . '/pear/db',
 );
