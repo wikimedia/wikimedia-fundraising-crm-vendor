7ecd33ce7b620918c5e0589c164a498e6b7269ce
eileen <emcnaughton@wikimedia.org>
2026-01-19 17:01:19 +1300

    wikimedia/smash-pig (v1.1.0)
    Change-Id: I66108e3b7d0717715e9107a0f0c2a4226771fe27

---
+++ wikimedia/smash-pig/PaymentProviders/dlocal/Audit/BaseParser.php
@@ -0,0 +1,39 @@
+<?php
+declare( strict_types=1 );
+
+namespace SmashPig\PaymentProviders\dLocal\Audit;
+
+class BaseParser {
+
+	protected array $row;
+	protected array $headers;
+
+	public function __construct( array $row, array $headers ) {
+		$this->row = $row;
+		$this->headers = $headers;
+	}
+
+	protected function getContributionTrackingId(): string {
+		$parts = explode( '.', $this->getOrderId() );
+		return $parts[0];
+	}
+
+	protected function isFromOrchestrator( $invoice ): bool {
+		// ignore gravy transactions, they have no period and contain letters
+		return ( !strpos( $invoice, '.' ) && !is_numeric( $invoice ) );
+	}
+
+	protected function getOrderId(): string {
+		foreach ( [ 'TRANSACTION_ID', 'DESCRIPTION', 'Transaction Invoice', 'Invoice' ] as $field ) {
+			$value = trim( (string)( $this->row[$field] ?? '' ) );
+			if ( $value === '' ) {
+				continue;
+			}
+			if ( preg_match( '/^[0-9]+(\.[0-9]+)?$/', $value ) === 1 ) {
+				return $value;
+			}
+		}
+		return '';
+	}
+
+}
